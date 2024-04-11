9156d4100651dab165e6039494c47f84020b2958
thaipq <pqthai0294@gmail.com>
2024-04-11 13:10:42 +0700

    Màn hình user chỉnh sửa

--- theme/js/plugins-init/datatables.init.js
+++ theme/js/plugins-init/datatables.init.js
@@ -2,51 +2,60 @@
 (function($) {
     "use strict"
     //example 1
-    var table = $('#example').DataTable({
-        createdRow: function ( row, data, index ) {
-           $(row).addClass('selected')
-        } 
+    var table = $('#tbl-user').DataTable({
+        dom: '<"float-left spacing"f><"float-left spacing"l><"pull-right"B><t><"pull-left"i><"pull-right"p>', // Thứ tực dom pull-left: filter, t: table, pull-right: pagination
+        //lengthChange:false, // Ẩn số dòng trên 1 trang
+        //info: false, // Ẩn thông tin số dòng
+        buttons: [ // Thêm nút
+            {
+                text: 'Thêm',
+                className: 'btn btn-outline-primary spacing',
+                action: function (e, dt, node, config) {
+                    $(node).attr({
+                        'data-toggle': 'modal',
+                        'data-target': '#exampleModalCenter',
+                        'data-whatever': 'myValue'
+                    });
+                }
+            },
+            {
+                extend: 'excel', // Xuất ra file excel
+                text: 'Export Excel',
+                className: 'btn btn-outline-primary spacing excelButton',
+                exportOptions: {
+                    modifier: {
+                        page: 'current'
+                    }
+                }
+            },
+            {
+                text: 'Import Excel',
+                className: 'btn btn-outline-primary spacing',
+                action: function (e, dt, node, config) {
+                    $(node).attr({
+                        'data-toggle': 'modal',
+                        'data-target': '#importExcel',
+                        'data-whatever': ''
+                    });
+                }
+            },
+        ],
+        stripeClasses: ['table-row-even', 'table-row-odd'], // Thêm class cho từng dòng
+        language: { // Ngôn ngữ
+            paginate: {
+                previous: "<i class='fa fa-angle-left'></i>",
+                next: "<i class='fa fa-angle-right'></i>"
+            }
+        }
     });
-      
-    table.on('click', 'tbody tr', function() {
-    var $row = table.row(this).nodes().to$();
-    var hasClass = $row.hasClass('selected');
-    if (hasClass) {
-        $row.removeClass('selected')
-    } else {
-        $row.addClass('selected')
-    }
-    })
+
+    table.on('click', 'tbody tr', function(e) {
+        if (!$(e.target).hasClass('fa-ellipsis-h')) {
+            $('#exampleModalCenter').modal('show');
+        }
+    });
     
     table.rows().every(function() {
-    this.nodes().to$().removeClass('selected')
-    });
-
-
-
-    //example 2
-    var table2 = $('#example2').DataTable( {
-        createdRow: function ( row, data, index ) {
-            $(row).addClass('selected')
-        },
-
-        "scrollY":        "42vh",
-        "scrollCollapse": true,
-        "paging":         false
-    });
-
-    table2.on('click', 'tbody tr', function() {
-        var $row = table2.row(this).nodes().to$();
-        var hasClass = $row.hasClass('selected');
-        if (hasClass) {
-            $row.removeClass('selected')
-        } else {
-            $row.addClass('selected')
-        }
-    })
-        
-    table2.rows().every(function() {
-        this.nodes().to$().removeClass('selected')
     });
    
 })(jQuery);
